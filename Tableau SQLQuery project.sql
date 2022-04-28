d2bb9309f2ef2f1d22333c069e52eb67a3a22fba
Pranay Reddy <103221446+pranayreddy1506@users.noreply.github.com>
2022-04-28 21:52:37 +0530

    Add files via upload

---
+++ Tableau SQLQuery project.sql
@@ -0,0 +1,24 @@
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProjects..CovidDeaths
+where continent is not null 
+order by 1,2
+
+-- take these out as they are not inluded in the above queries and want to stay consistent
+-- European Union is part of Europe
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From PortfolioProjects..CovidDeaths
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProjects..CovidDeaths
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProjects..CovidDeaths
+Group by Location, Population, date
+order by PercentPopulationInfected desc
